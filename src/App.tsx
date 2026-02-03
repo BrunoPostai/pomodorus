0bfd1c82b41504d0ce166034c1f2d39582c6d42d
Bruno Postai <cechinelbruno@gmail.com>
2026-02-03 23:27:12 +0100

    feature(components/DefaultInput) brand new input component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 import { Countdown } from "./components/Countdown";
+import { DefaultInput } from "./components/DefaultInput";
 
 function App() {
   return (
@@ -18,6 +19,27 @@ function App() {
 
       <Container>
         <Countdown />
+      </Container>
+
+      <Container>
+        <form className="form" action="">
+          <div className="formRow">
+            <DefaultInput id="meuInput" type='text' placeholder="digite algo"/>
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
