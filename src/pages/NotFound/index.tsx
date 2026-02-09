4fdda63879f402b9bf82f08e362ff154bc8ff25f
Bruno Postai <cechinelbruno@gmail.com>
2026-02-09 14:44:43 +0100

    feature(components/GenericHtml) generic html component

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,10 +1,31 @@
+import { Container } from "../../components/Container";
+import { GenericHtml } from "../../components/GenericHtml";
 import MainTemplate from "../../templates/MainTemplate";
 
 export function Home() {
   return (
     <>
       <MainTemplate>
-        <h2>NOT FOUND</h2>
+        <Container>
+          <GenericHtml>
+            <p>
+              Mas calma, você não está perdido no espaço (ainda). Dá pra voltar
+              em segurança para a <a href="/">página principal</a> ou{" "}
+              <a href="/history">para o histórico</a> — ou pode ficar por aqui e
+              fingir que achou uma página secreta que só os exploradores mais
+              legais conseguem acessar. 🧭✨
+            </p>
+            <p>
+              Se você acha que essa página deveria existir (ou se quiser bater
+              um papo sobre viagem no tempo e buracos de minhoca), é só entrar
+              em contato. Caso contrário, use o menu para voltar ao mundo real.
+            </p>
+            <p>
+              Enquanto isso, fica aqui uma reflexão: "Se uma página não existe
+              na internet, será que ela existiu de verdade?" 🤔💭
+            </p>
+          </GenericHtml>
+        </Container>
       </MainTemplate>
     </>
   );
