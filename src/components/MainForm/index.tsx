0883520414744b877eb95f06d183c2542a21efcc
Bruno Postai <cechinelbruno@gmail.com>
2026-02-12 23:42:43 +0100

    feature(src/utils) formatSecondsToMinutes function to use on formattedSecondsRemaining output

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -7,6 +7,7 @@ import type { TaskModel } from "../../models/TaskModel";
 import { useTaskContext } from "../../contexts/TaskContext/UseTaskContext";
 import { getNextCycle } from "../../utils/getNextCycle";
 import { getNextCycleType } from "../../utils/getNextCycleType";
+import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
 
 export function MainForm() {
   const { state, setState } = useTaskContext();
@@ -46,7 +47,7 @@ export function MainForm() {
         activeTask: newTask,
         currentCycle: nextCycle,
         secondsRemaining, //conferir
-        formattedSecondsRemaining: "00:00", //conferir
+        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining), //conferir
         tasks: [...prevState.tasks, newTask],
       };
     });
