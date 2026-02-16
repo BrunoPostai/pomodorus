400f85ab987e1870f7a54d688b45f9e520d19d09
Bruno Postai <cechinelbruno@gmail.com>
2026-02-16 15:51:48 +0100

    feature(components/menu) now the menu button can interrupt the task.

--- src/components/Cycles/index.tsx
+++ src/components/Cycles/index.tsx
@@ -14,8 +14,6 @@ export function Cycles() {
     longBreakTime: "long-break",
   };
 
-  console.log(cycleStep);
-
   return (
     <div className={styles.cycles}>
       <span>Ciclos:</span>
--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,4 +1,4 @@
-import { PlayCircleIcon } from "lucide-react";
+import { PlayCircleIcon, StopCircleIcon } from "lucide-react";
 import { DefaultInput } from "../DefaultInput";
 import { DefaultButton } from "../DefaultButton";
 import { Cycles } from "../Cycles";
@@ -53,6 +53,17 @@ export function MainForm() {
     });
   }
 
+  function handleInterruptTask() {
+    setState((prevState) => {
+      return {
+        ...prevState,
+        activeTask: null,
+        secondsRemaining: 0, //conferir
+        formattedSecondsRemaining: "00:00", //conferir
+      };
+    });
+  }
+
   return (
     <form onSubmit={handleCreateNewTask} className="form" action="">
       <div className="formRow">
@@ -76,7 +87,26 @@ export function MainForm() {
       )}
 
       <div className="formRow">
-        <DefaultButton icon={<PlayCircleIcon />} color="red" />
+        {!state.activeTask ? (
+          <DefaultButton
+            type="submit"
+            icon={<PlayCircleIcon />}
+            color="green"
+            aria-label="Start new chore"
+            title="Start new chore"
+            key="submit_btn"
+          />
+        ) : (
+          <DefaultButton
+            type="button"
+            icon={<StopCircleIcon />}
+            color="red"
+            aria-label="Interrupt chore"
+            title="Interrupt chore"
+            onClick={handleInterruptTask}
+            key="button_btn"
+          />
+        )}
       </div>
     </form>
   );
