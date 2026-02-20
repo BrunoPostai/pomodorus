12cde9d76b1f18b275dcbff91caa41ae7297fd44
Bruno Postai <cechinelbruno@gmail.com>
2026-02-20 20:21:30 +0100

    feature(context) now the countdown updates every second on state and page

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -3,6 +3,7 @@ import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 import { taskReducer } from "./taskReducer";
 import { TimerWorkerManager } from "../../workers/timerWorkerManager";
+import { TaskActionsTypes } from "./taskActions";
 
 type taskContextProviderProps = {
   children: React.ReactNode;
@@ -18,12 +19,21 @@ export function TaskContextProvider({ children }: taskContextProviderProps) {
     console.log(countDownSeconds);
 
     if (countDownSeconds <= 0) {
-      console.log("Worker COMPLETED");
+      dispatch({
+        type: TaskActionsTypes.COMPLETE_TASK,
+      });
       worker.terminate();
+    } else {
+      dispatch({
+        type: TaskActionsTypes.COUNT_DOWN,
+        payload: { secondsRemaining: countDownSeconds },
+      });
     }
   });
 
   useEffect(() => {
+    console.log("TaskContextProvider", state);
+
     if (!state.activeTask) {
       console.log("Worker terminado por falta de activeTask");
       worker.terminate();
--- src/contexts/TaskContext/taskActions.ts
+++ src/contexts/TaskContext/taskActions.ts
@@ -4,19 +4,30 @@ export enum TaskActionsTypes {
   START_TASK = "START_TASK",
   INTERRUPT_TASK = "INTERRUPT_TASK",
   RESET_STATE = "RESET_STATE",
+  COUNT_DOWN = "COUNT_DOWN",
+  COMPLETE_TASK = "COMPLETE_TASK",
 }
 
-export type TaskActionsWithPayload = {
-  type: TaskActionsTypes.START_TASK;
-  payload: TaskModel;
-};
+export type TaskActionsWithPayload =
+  | {
+      type: TaskActionsTypes.START_TASK;
+      payload: TaskModel;
+    }
+  | {
+      type: TaskActionsTypes.COUNT_DOWN;
+      payload: { secondsRemaining: number };
+    };
 
 export type TaskActionsWithoutPayload =
   | {
+      payload: any;
       type: TaskActionsTypes.RESET_STATE;
     }
   | {
       type: TaskActionsTypes.INTERRUPT_TASK;
+    }
+  | {
+      type: TaskActionsTypes.COMPLETE_TASK;
     };
 
 export type TaskActionsModel =
