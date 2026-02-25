da01ce3050527313c196c2e847f0432373cd2417
Bruno Postai <cechinelbruno@gmail.com>
2026-02-25 17:33:04 +0100

    feature(History)now history will display real data

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -4,8 +4,11 @@ import { DefaultButton } from "../../components/DefaultButton";
 import { Header } from "../../components/Header";
 import MainTemplate from "../../templates/MainTemplate";
 import styles from "./styles.module.css";
+import { useTaskContext } from "../../contexts/TaskContext/UseTaskContext";
 
 export function History() {
+  const { state } = useTaskContext();
+
   return (
     <>
       <MainTemplate>
@@ -35,14 +38,14 @@ export function History() {
                 </tr>
               </thead>
               <tbody>
-                {Array.from({ length: 20 }).map((_, index) => {
+                {state.tasks.map((task) => {
                   return (
-                    <tr key={index}>
-                      <td>Estudar</td>
-                      <td>25min</td>
-                      <td>20/04/2025 08:00</td>
-                      <td>Completa</td>
-                      <td>Foco</td>
+                    <tr key={task.id}>
+                      <td>{task.name}</td>
+                      <td>{task.duration}</td>
+                      <td>{new Date(task.startDate).toISOString()}</td>
+                      <td>{task.interruptDate}</td>
+                      <td>{task.type}</td>
                     </tr>
                   );
                 })}
